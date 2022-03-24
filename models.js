6d6eeda6a572c53ae717f602f9c690fcb5f5d3b0
jtsapos <jtsapos@gmx.com>
2022-03-24 12:22:33 -0400

    Updated models.js

--- models.js
+++ models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); //to hash users’ passwords and compare hashed passwords every time users log in in order to ensure a more secure login authentication process.
+
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
@@ -21,6 +23,20 @@ let movieSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
+
+  //function which does the actual hashing of submitted passwords.
+  userSchema.statics.hashPassword = (password) => { 
+    return bcrypt.hashSync(password, 10);
+  };
+  //function that compares submitted hashed passwords with the hashed passwords stored in your database
+  //Don't use arrow functions when defining instance methods. validatePassword is an example of an instance method, 
+  //a method that can be called on each object/document created (each individual object/document). 
+  //Arrow functions bind the 'this' keyword to the object that owns that function, which in this case, is userSchema.methods—NOT user, even when validatePassword is being called on user within the line:
+  //if(!!user.validatePassword(password))
+  userSchema.methods.validatePassword = function(password) { 
+    return bcrypt.compareSync(password, this.Password);
+  };
+
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
 
